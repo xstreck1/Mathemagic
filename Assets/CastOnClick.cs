ba1a9439b2e89ac6883d9db9da8c68d628830edd
Adam Streck <adam.streck@gmail.com>
2015-02-07 00:11:53 +0100

    Player movement.

--- Assets/CastOnClick.cs
+++ Assets/CastOnClick.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class CastOnClick : MonoBehaviour {
+    bool _spell = true;
+    Vector3 _initial_postion;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        _initial_postion = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetMouseButtonDown(1))        {
+            Debug.Log("Spell switch.");
+            _spell = !_spell;
+        }
+    }
+
+    // 
+    void OnMouseDown() {
+        Debug.Log("spell casted on " + this.name);
+
+        if (_spell)
+            transform.Translate(new Vector3(2f, 0, 0));
+        else
+            transform.RotateAround(_initial_postion, new Vector3(0f, 0f, 1f), 90f);
+    }
 }
---
+++ Assets/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+    float _dist_to_ground = 1f;
+    public float _MOVE_SPEED = 200f;
+    public float _JUMP_FORCE = 500f;
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, _dist_to_ground + 0.1f);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rigidbody.AddForce(Vector3.right * _MOVE_SPEED);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rigidbody.AddForce(Vector3.left * _MOVE_SPEED);
+        }
+        if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
+        {
+            rigidbody.AddForce(Vector3.up * _JUMP_FORCE);
+
+        }
+    }
+}
