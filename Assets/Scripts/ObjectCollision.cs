e6f2061a890960d8625d6d8dc9fee431a501981c
Adam Streck <adam.streck@gmail.com>
2015-02-07 16:56:47 +0100

    Level built using prefabs.

--- Assets/Scripts/ObjectCollision.cs
+++ Assets/Scripts/ObjectCollision.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ObjectCollision : MonoBehaviour {
+public class ObjectCollision : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
 }
