7effe90c1047413fbc0f9afe6de6f000d90825b0
Adam Streck <adam.streck@gmail.com>
2015-02-07 11:39:00 +0100

    Improved player movement.

--- Assets/CastOnClick.cs
+++ Assets/CastOnClick.cs
@@ -1,30 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CastOnClick : MonoBehaviour {
+public class CastOnClick : MonoBehaviour
+{
     bool _spell = true;
     Vector3 _initial_postion;
 
-	// Use this for initialization
-	void Start () {
+    Matrix4x4 _move_to_center;
+    Matrix4x4 _move_from_center;
+    Matrix4x4 _translate_mat;
+    Matrix4x4 _rotate_mat;
+
+    // Use this for initialization
+    void Start()
+    {
+        _move_to_center = Matrix4x4.TRS(-transform.position, Quaternion.identity, Vector3.one);
+        _move_from_center = Matrix4x4.TRS(transform.position, Quaternion.identity, Vector3.one);
         _initial_postion = transform.position;
+        _translate_mat = Matrix4x4.TRS(new Vector3(2f, 0f, 0f), Quaternion.identity, Vector3.one);
+        _rotate_mat = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, 30f), Vector3.one);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(1))        {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
             Debug.Log("Spell switch.");
             _spell = !_spell;
         }
     }
 
+    void updateCoordinates(Matrix4x4 matrix)  {
+        matrix = _move_from_center * matrix * _move_to_center;
+        transform.position = matrix.MultiplyPoint(transform.position);
+        transform.up = (matrix.MultiplyVector(transform.up));
+
+    }
+
     // 
-    void OnMouseDown() {
+    void OnMouseDown()
+    {
         Debug.Log("spell casted on " + this.name);
 
         if (_spell)
-            transform.Translate(new Vector3(2f, 0, 0));
+        {
+            updateCoordinates(_rotate_mat);
+            // updateCoordinates(_translate_mat);
+        }
+
         else
-            transform.RotateAround(_initial_postion, new Vector3(0f, 0f, 1f), 90f);
+        {
+            updateCoordinates(_translate_mat * _rotate_mat);
+        }
     }
 }
--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class CharacterController : MonoBehaviour
 {
     float _dist_to_ground = 1f;
-    public float _MOVE_SPEED = 200f;
-    public float _JUMP_FORCE = 500f;
+    public float _MOVE_FORCE = 750f;
+    public float _MAX_SPEED = 5f;
+    public float _JUMP_FORCE = 750f;
 
     bool IsGrounded()
     {
@@ -15,19 +16,36 @@ public class CharacterController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        // Debug.Log("velocity " + rigidbody.velocity.x);
+        if (Input.GetKey(KeyCode.D))
         {
-            rigidbody.AddForce(Vector3.right * _MOVE_SPEED);
+            if (rigidbody.velocity.x < _MAX_SPEED)
+            {
+                rigidbody.AddForce(Vector3.right * _MOVE_FORCE * Time.deltaTime);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        else if(Input.GetKey(KeyCode.A))
         {
-            rigidbody.AddForce(Vector3.left * _MOVE_SPEED);
+            if (rigidbody.velocity.x > -_MAX_SPEED)
+            {
+                rigidbody.AddForce(Vector3.left * _MOVE_FORCE * Time.deltaTime);
+            }
+        }
+        else // Apply breaking in the opposing direction of movement (slow down)
+        {
+            float old_velocity = rigidbody.velocity.x;
+            rigidbody.AddForce((rigidbody.velocity.x > 0 ? Vector3.left : Vector3.right) * _MOVE_FORCE * Time.deltaTime);
+            if (Mathf.Sign(old_velocity) != Mathf.Sign(rigidbody.velocity.x))
+            {
+                Vector3 new_vleocity = rigidbody.velocity;
+                new_vleocity.x = 0f;
+                rigidbody.velocity = new_vleocity;
+            }
         }
         if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
         {
